cbbf8a98013808643023a33d1ff4c277e0adb120
gkokhabi <91689217+gkokhabi@users.noreply.github.com>
2021-11-12 21:24:00 -0500

    draft 1

---
+++ body.js
@@ -0,0 +1,21 @@
+document.body.style.cursor = 'none';
+$(document).keydown(function(e) {
+  if (e.keyCode == 27 | e.keyCode == 122) return false;
+});
+
+  setInterval(function(){
+    w = Math.floor(Math.random()*($(window).outerWidth()-0+ 1))-150;
+		h = Math.floor(Math.random()*($(window).outerHeight()-0+ 1))-150;
+    $('.mouse').animate({
+      top	: h+10,
+			left: w+820
+		}, 750);
+    $('body').append('<div class="error" style="top:' + h + 'px; left:' + w + 'px;"></div>');
+	}, 300);
+
+
+  window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+  });
