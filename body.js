78b25763a1542fc93d2c945d092c0238646ed7f0
gkokhabi <91689217+gkokhabi@users.noreply.github.com>
2021-11-19 10:20:11 -0500

    Update body.js

--- body.js
+++ body.js
@@ -11,11 +11,18 @@ $(document).keydown(function(e) {
 			left: w+820
 		}, 750);
     $('body').append('<div class="error" style="top:' + h + 'px; left:' + w + 'px;"></div>');
-	}, 300);
+	}, 250);
 
+  
+  
+  var message = "Right-click disabled"; 
+  function clickIE() {if (document.all) {(message);return false;}} 
+  function clickNS(e) {if 
+  (document.layers||(document.getElementById&&!document.all)) { 
+  if (e.which==2||e.which==3) {(message);return false;}}} 
+  if (document.layers) {document.captureEvents(Event.MOUSEDOWN);document.onmousedown = clickNS;} 
+  else{document.onmouseup = clickNS;document.oncontextmenu = clickIE;} 
+  document.oncontextmenu = new Function("return false");
+  
+  
 
-  window.addEventListener("DOMContentLoaded", event => {
-    const audio = document.querySelector("audio");
-    audio.volume = 0.2;
-    audio.play();
-  });
